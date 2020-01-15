e5cd82766752a718b9382028938dc75dde2636cc
miyazaki761 <miyazaki.a0806@gmail.com>
2020-01-15 10:37:47 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreplys@example.com'
   layout 'mailer'
 end
---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  test "account_activatioin" do
+    mail = UserMailer.account_activatioin
+    assert_equal "Account activatioin", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "password_reset" do
+    mail = UserMailer.password_reset
+    assert_equal "Password reset", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
