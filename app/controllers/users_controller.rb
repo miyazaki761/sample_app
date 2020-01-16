e9782e5236cedb5334cd3711dabfe5ab017a1d95
miyazaki761 <miyazaki.a0806@gmail.com>
2020-01-16 13:55:57 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILLIN
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
